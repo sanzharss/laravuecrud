cf0ee8733e585dcfea05138a2ae4f68e0445c5d3
Sanzhar <s.serikbayev@enpf.kz>
2021-01-26 22:31:20 +0600

    first commit

---
+++ app/Company.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $fillable = ['name', 'address', 'website', 'email'];
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('website')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+}
